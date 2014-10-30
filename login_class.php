5381f92fde446214fed92a0a691e2beebf815b27
Jaume Valls <jaumevalls@gmail.com>
2014-10-30 12:52:26 +0000

    Add the class file to manage the login system

---
+++ login_class.php
@@ -0,0 +1,13 @@
+<?
+
+/* class engine page
+*  Written by:     Jaume Valls
+*  First Release:  
+*  Description: Class file to manage the login sstem
+*/
+
+include_once 'conf/psl-conf.php';
+
+
+
+?>
