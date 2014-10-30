3a539cdf62ea99858f9091a7f851f8a7b8b958bd
Jaume Valls <jaumevalls@gmail.com>
2014-10-30 15:05:44 +0000

    Class construct created and properties set up

--- login_class.php
+++ login_class.php
@@ -9,26 +9,60 @@
 include_once 'conf/psl-config.php';
 
 class login {
-    
+
     protected static $connection;
+    private $hasDB;
+
+    var $perms = array();       //Array : Stores the permissions for the user
+    var $userID = 0;            //Integer : Stores the ID of the current user
+    var $userRoles = array();   //Array : Stores the roles of the current user
+
+
+    function __construct ($userID = '')
+    {
+        session_start();
+        ob_start();
+        
+        if ($userID != '')
+        {
+            $this->userID = floatval($userID);
+        }
+        else
+        {
+            $this->userID = floatval($_SESSION['userID']);
+        }
+        
+        $this->userRoles = $this->getUserRoles('ids');
+        $this->buildACL();
+    }
     
+    function login ($userID='')
+    {
+        $this->__construct($userID);
+    }
+
     public function connectDb ()
     {
         if (isset(self::$connection))
         {
             self::$connection = new mysqli(HOST,USER,PASSWORD,DATABASE);
         }
-        if (self::$connection->connect_error)
+        if (!is_resource(self::$connection))
         {
+            $this->hasDB = false;
             die("Could not connect to the MySQL server at localhost.");
         }        
-        
+
+        $this->hasDB = true;
         return self::$connection;
     }
-    
-    
-    
-    
+
+    function getUsersRoles()
+    {
+        
+    }
+
+
 }
 
 ?>
